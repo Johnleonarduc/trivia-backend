ede0b33ac884457fb7101963993a3882c00de8e0
UMUNNAKWE, JOHNLEONARD CHINAEMEREM <johnleonarduc@gmail.com>
2022-08-30 11:11:32 +0100

    update files

--- models.py
+++ models.py
@@ -12,13 +12,17 @@ db_config = dotenv_values(".env")
 DATABASE_URL = os.environ['DATABASE_URL']
 # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 db = SQLAlchemy()
 
 """
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 """
-def setup_db(app, database_path=DATABASE_URL):
+def setup_db(app, database_path=uri):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
