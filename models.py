31a2cd48c873a43af35b7ea8a9d83649680d13e1
UMUNNAKWE, JOHNLEONARD CHINAEMEREM <johnleonarduc@gmail.com>
2022-08-30 02:46:05 +0100

    provision db in models.py

--- models.py
+++ models.py
@@ -1,12 +1,16 @@
 import os
+# import psycopg2
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import dotenv_values
 
 db_config = dotenv_values(".env")
 
-database_name = 'trivia'
-database_path = 'postgresql://{}:{}@{}/{}'.format(db_config['USERNAME'], db_config['PASSWORD'], 'localhost:5432', database_name)
+# database_name = 'trivia'
+# database_path = 'postgresql://{}:{}@{}/{}'.format(db_config['USERNAME'], db_config['PASSWORD'], 'localhost:5432', database_name)
+
+DATABASE_URL = os.environ['DATABASE_URL']
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 db = SQLAlchemy()
 
@@ -14,7 +18,7 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 """
-def setup_db(app, database_path=database_path):
+def setup_db(app, database_path=DATABASE_URL):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
